495b45a886164751c4d1f8dd763f1bfc934e2b03
jbweiss <josef@josefweiss.com>
2016-01-10 22:28:08 -0500

    -

---
+++ comments.py
@@ -0,0 +1,15 @@
+# Comments in python.  As you may have notice this line starts with a hash sign (#).
+
+# Hash signs are not processed by the python interpreter. You can use hash signs to create comments
+# in your program to explain specific parts of the program, to creates notes, or to troubleshoot and/or
+# test parts of the program.
+
+# In the example below, the first line and third line will be printed, and the second line will be ignored.
+
+print('This is the first line')
+# print('This is the second line')
+print('This is the third line')
+
+
+# After running the program, remove the hash sign and run the program again.
+
