e265fa4e6647e69a57775ff79bfd38e127e7a7c8
jbweiss <josef@josefweiss.com>
2016-01-10 22:10:23 -0500

    Print Function

---
+++ print_function.py
@@ -0,0 +1,6 @@
+# The print() function is a built in function within python. Strings framed in either double or single
+# quotes is valid.  In the code below, the parameter for the print function is entered within the paranthesis of
+# the print function.  Since the text is technically a string, it is framed in quotes.  Simply running this
+# program will result in the printing of the message shown.
+
+print('My first python message!')
