807605319324ae4c5a6ef0cbb7062800f12e23e3
jbweiss <josef@josefweiss.com>
2016-01-20 18:40:12 -0500

    -

---
+++ Lec2.6.2.py
@@ -0,0 +1,13 @@
+# Lec 2.6, slide 2
+
+# Bool Test
+
+x = int(raw_input('Enter an integer: '))
+# If the remainder of x / 2 = 0 then print Even
+if x%2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
