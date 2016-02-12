19b5de2d9a89c364ed44e3e20dc00ca258d5eb52
jbweiss <josef@josefweiss.com>
2016-02-11 22:20:46 -0500

    -

--- Problems.py
+++ Problems.py
@@ -1,20 +1,8 @@
 
 
-balance = 4773
-annualInterestRate = 0.2
-monthlyInterestRate = annualInterestRate / 12
-monthlyPayment = 10
-newBalance = balance - 10
+testList = [1, -4, 8, -9]
 
-while newBalance > 0:
-    monthlyPayment += 10
-    newBalance = balance
-    month = 0
 
-    while month < 12 and newBalance > 0:
-        newBalance -= monthlyPayment
-        interest = monthlyInterestRate * newBalance
-        newBalance += interest
-        month += 1
-    newBalance = round(newBalance,2)
-print 'Lowest Payment:', monthlyPayment
+def timeSelf(x):
+    return x*x
+applyToEach(testList, timeSelf)
